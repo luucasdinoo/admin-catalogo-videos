8782ac0b21222fb60c6d5fdbedcd46cb2cd9da75
Lucas Bernadino <luucasdinoo@gmail.com>
2025-01-28 14:11:43 -0300

    Implementing Category With DDD

---
+++ domain/src/main/java/com/dino/admin/catalogo/domain/category/Category.java
@@ -0,0 +1,91 @@
+package com.dino.admin.catalogo.domain.category;
+
+import com.dino.admin.catalogo.domain.AggregateRoot;
+import com.dino.admin.catalogo.domain.validation.ValidationHandler;
+
+import java.time.Instant;
+
+public class Category extends AggregateRoot<CategoryId> {
+
+    private String name;
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    private Category(
+                    final CategoryId anId,
+                    final String aName,
+                    final String aDescription,
+                    final boolean isActive,
+                    final Instant aCreatedAt,
+                    final Instant aUpdatedAt,
+                    final Instant aDeletedAt
+    ) {
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = aUpdatedAt;
+        this.deletedAt = aDeletedAt;
+    }
+
+    public static Category newCategory(final String aName, final String aDescription, final boolean isActive){
+        final CategoryId id = CategoryId.unique();
+        final Instant now = Instant.now();
+        final var deletedAt = isActive ? null : now;
+        return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
+    }
+
+    @Override
+    public void validate(final ValidationHandler handler) {
+        new CategoryValidator(this, handler).validate();
+    }
+
+    public Category activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = Instant.now();
+        }
+
+        this.active = false;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public CategoryId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+}
---
+++ domain/src/main/java/com/dino/admin/catalogo/domain/category/CategoryId.java
@@ -0,0 +1,44 @@
+package com.dino.admin.catalogo.domain.category;
+
+import com.dino.admin.catalogo.domain.Identifier;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CategoryId extends Identifier {
+
+    private final String value;
+
+    private CategoryId(final String value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    public static CategoryId unique(){
+        return new CategoryId(UUID.randomUUID().toString().toLowerCase());
+    }
+
+    public static CategoryId from(final String anId) {
+        return new CategoryId(anId);
+    }
+
+    public static CategoryId from(final UUID anId) {
+        return new CategoryId(anId.toString().toLowerCase());
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryId that = (CategoryId) o;
+        return Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getValue());
+    }
+}
