6b260589b27ddbe082e76841a4c128c5ae6e7739
Lucas Bernadino <luucasdinoo@gmail.com>
2025-01-29 19:19:30 -0300

    Initial config infrastructure module

--- infrastructure/src/main/java/com/dino/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/dino/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,13 @@
 package com.dino.admin.catalogo.infrastructure;
 
+import com.dino.admin.catalogo.infrastructure.config.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/dino/admin/catalogo/infrastructure/config/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.dino.admin.catalogo.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dino.admin.catalogo")
+public class WebServerConfig {
+}
